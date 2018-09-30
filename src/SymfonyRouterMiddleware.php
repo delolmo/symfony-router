e7aa405ac868c4804ffd0c6f3a61aebf511d06eb
Antonio del Olmo <adelolmog@gmail.com>
2018-09-30 12:43:01 +0200

    Create SymfonyRouterMiddleware

---
+++ src/SymfonyRouterMiddleware.php
@@ -0,0 +1,79 @@
+<?php
+declare(strict_types = 1);
+
+namespace DelOlmo\Middleware;
+
+use Middlewares\Utils\Traits\HasResponseFactory;
+use Psr\Http\Message\ResponseInterface as Response;
+use Psr\Http\Message\ServerRequestInterface as Request;
+use Psr\Http\Server\MiddlewareInterface as Middleware;
+use Psr\Http\Server\RequestHandlerInterface as Handler;
+use Symfony\Bridge\PsrHttpMessage\Factory\HttpFoundationFactory;
+use Symfony\Component\Routing\Exception\ResourceNotFoundException;
+use Symfony\Component\Routing\Exception\MethodNotAllowedException;
+use Symfony\Component\Routing\RouterInterface as Router;
+
+/**
+ * @author Antonio del Olmo García <adelolmog@gmail.com>
+ */
+class SymfonyRouterMiddleware implements Middleware
+{
+
+    use HasResponseFactory;
+
+    /**
+     * @var string
+     */
+    private $attribute = '_route';
+
+    /**
+     * @var Symfony\Component\Routing\RouterInterface
+     */
+    private $router;
+
+    /**
+     * @param Symfony\Component\Routing\RouterInterface $router
+     */
+    public function __construct(Router $router)
+    {
+        $this->router = $router;
+    }
+
+    /**
+     * Process a request and return a response.
+     */
+    public function process(Request $request, Handler $handler): Response
+    {
+        try {
+            $symfonyRequest = (new HttpFoundationFactory())
+                ->createRequest($request);
+            $route = $this->router
+                ->matchRequest($symfonyRequest);
+        } catch (ResourceNotFoundException $e) {
+            return $this->createResponse(404);
+        } catch (MethodNotAllowedException $e) {
+            $allows = implode(', ', $e->getAllowedMethods());
+            return $this->createResponse(405)
+                    ->withHeader('Allow', $allows);
+        } catch (\Exception $e) {
+            return $this->createResponse(500);
+        }
+
+        $request = $request->withAttribute($this->attribute, $route);
+
+        return $handler->handle($request);
+    }
+
+    /**
+     * Set the attribute name to store the route reference.
+     *
+     * @param string $attribute
+     * @return \self
+     */
+    public function setAttribute(string $attribute): self
+    {
+        $this->attribute = $attribute;
+        return $this;
+    }
+
+}
---
+++ tests/SymfonyRouterMiddlewareTest.php
@@ -0,0 +1,149 @@
+<?php
+
+declare(strict_types = 1);
+
+namespace DelOlmo\Middleware;
+
+use DelOlmo\Middleware\SymfonyRouterMiddleware;
+use Middlewares\Utils\Dispatcher;
+use Middlewares\Utils\Factory;
+use PHPUnit\Framework\TestCase;
+use Symfony\Bridge\PsrHttpMessage\Factory\HttpFoundationFactory;
+use Symfony\Component\Routing\Matcher\UrlMatcher;
+use Symfony\Component\Routing\RequestContext;
+use Symfony\Component\Routing\RouteCollection;
+use Symfony\Component\Routing\Route;
+use Symfony\Component\Routing\Router;
+
+/**
+ * @author Antonio del Olmo García <adelolmog@gmail.com>
+ */
+class SymfonyRouterMiddlewareTest extends TestCase
+{
+
+    /**
+     *
+     * @var \Symfony\Component\Routing\RouteCollection
+     */
+    private $routes = null;
+
+    /**
+     *
+     * @var \Symfony\Component\Routing\Router
+     */
+    private $router = null;
+
+    /**
+     *
+     * @var \Symfony\Component\Config\Loader\LoaderInterface
+     */
+    private $loader = null;
+
+    /**
+     * {@inheritdoc}
+     */
+    protected function setUp()
+    {
+        $this->loader = $this->getMockBuilder('Symfony\Component\Config\Loader\LoaderInterface')->getMock();
+        $this->router = new Router($this->loader, 'routing.yml');
+
+        $this->routes = new RouteCollection();
+        $this->routes->add('test', new Route('/users', [], [], [], '', [], ['GET']));
+    }
+
+    /**
+     *
+     */
+    public function testResourceNotFoundException()
+    {
+        $request = Factory::createServerRequest('GET', '/posts');
+
+        $factory        = new HttpFoundationFactory();
+        $symfonyRequest = $factory->createRequest($request);
+        $context        = (new RequestContext())->fromRequest($symfonyRequest);
+        $matcher        = new UrlMatcher($this->routes, $context);
+        $p              = new \ReflectionProperty($this->router, 'matcher');
+        $p->setAccessible(true);
+        $p->setValue($this->router, $matcher);
+
+        $response = Dispatcher::run([
+                new SymfonyRouterMiddleware($this->router)
+                ], $request);
+
+        $this->assertEquals(404, $response->getStatusCode());
+    }
+
+    /**
+     *
+     */
+    public function testMethodNotAllowedException()
+    {
+        $request = Factory::createServerRequest('POST', '/users');
+
+        $factory        = new HttpFoundationFactory();
+        $symfonyRequest = $factory->createRequest($request);
+        $context        = (new RequestContext())->fromRequest($symfonyRequest);
+        $matcher        = new UrlMatcher($this->routes, $context);
+        $p              = new \ReflectionProperty($this->router, 'matcher');
+        $p->setAccessible(true);
+        $p->setValue($this->router, $matcher);
+
+        $response = Dispatcher::run([
+                new SymfonyRouterMiddleware($this->router)
+                ], $request);
+
+        $this->assertEquals(405, $response->getStatusCode());
+    }
+
+    /**
+     *
+     */
+    public function testRouteMatched()
+    {
+        $request = Factory::createServerRequest('GET', '/users');
+
+        $factory        = new HttpFoundationFactory();
+        $symfonyRequest = $factory->createRequest($request);
+        $context        = (new RequestContext())->fromRequest($symfonyRequest);
+        $matcher        = new UrlMatcher($this->routes, $context);
+        $p              = new \ReflectionProperty($this->router, 'matcher');
+        $p->setAccessible(true);
+        $p->setValue($this->router, $matcher);
+
+        $response = Dispatcher::run([
+                new SymfonyRouterMiddleware($this->router),
+                function($request) {
+                    echo $request->getAttribute('_route')['_route'];
+                }
+                ], $request);
+
+        $this->assertEquals('test', (string) $response->getBody());
+    }
+
+    /**
+     *
+     */
+    public function testCustomAttribute()
+    {
+        $request = Factory::createServerRequest('GET', '/users');
+
+        $factory        = new HttpFoundationFactory();
+        $symfonyRequest = $factory->createRequest($request);
+        $context        = (new RequestContext())->fromRequest($symfonyRequest);
+        $matcher        = new UrlMatcher($this->routes, $context);
+        $p              = new \ReflectionProperty($this->router, 'matcher');
+        $p->setAccessible(true);
+        $p->setValue($this->router, $matcher);
+
+        $response = Dispatcher::run([
+                    (new SymfonyRouterMiddleware($this->router))
+                    ->setAttribute('_route_test'),
+                function($request) {
+                    echo $request->getAttribute('_route_test')['_route'];
+                }
+                ], $request);
+
+        $this->assertEquals('test', (string) $response->getBody());
+    }
+
+}

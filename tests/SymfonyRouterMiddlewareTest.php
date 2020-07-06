7ddba23309314aa9c0f5c3c220259032e86861f5
Antonio del Olmo <adelolmog@gmail.com>
2020-07-06 16:01:02 +0200

    Optionally pass a custom ResponseFactoryInterface through the constructor (#9)
    Update `middlewares\utils` to `^3.0` and allow passing a custom ResponseFactoryInterface through the constructor. The default response factory will be created by the `Middlewares\Utils\Factory` static method `getResponseFactory`, preserving backwards compatibility.

--- tests/SymfonyRouterMiddlewareTest.php
+++ tests/SymfonyRouterMiddlewareTest.php
@@ -7,10 +7,12 @@ namespace DelOlmo\Middleware;
 use Middlewares\Utils\Dispatcher;
 use Middlewares\Utils\Factory;
 use PHPUnit\Framework\TestCase;
+use Psr\Http\Message\ResponseFactoryInterface;
 use ReflectionProperty;
 use Symfony\Bridge\PsrHttpMessage\Factory\HttpFoundationFactory;
 use Symfony\Component\Config\Loader\LoaderInterface;
 use Symfony\Component\Routing\Exception\NoConfigurationException;
+use Symfony\Component\Routing\Exception\ResourceNotFoundException;
 use Symfony\Component\Routing\Matcher\RequestMatcherInterface;
 use Symfony\Component\Routing\Matcher\UrlMatcher;
 use Symfony\Component\Routing\RequestContext;
@@ -33,6 +35,45 @@ class SymfonyRouterMiddlewareTest extends TestCase
 
         $this->routes = new RouteCollection();
         $this->routes->add('test', new Route('/users', [], [], [], '', [], ['GET']));
+    }
+
+    public function testNonDefaultResponseFactory() : void
+    {
+        $factory = $this->createMock(ResponseFactoryInterface::class);
+
+        $factory
+          ->expects(self::once())
+          ->method('createResponse')
+          ->with(404);
+
+        $context = $this->createMock(RequestContext::class);
+
+        $router = $this->createMock(Router::class);
+
+        $router
+            ->expects(self::once())
+            ->method('getContext')
+            ->willReturn($context);
+
+        $request = Factory::createServerRequest('GET', '/');
+
+        $symfonyRequest = (new HttpFoundationFactory())
+            ->createRequest($request);
+
+        $context
+            ->expects(self::once())
+            ->method('fromRequest')
+            ->with($symfonyRequest);
+
+        $router
+            ->expects(self::once())
+            ->method('matchRequest')
+            ->with($symfonyRequest)
+            ->will(self::throwException(new ResourceNotFoundException()));
+
+        $middleware = new SymfonyRouterMiddleware($router, $factory);
+
+        Dispatcher::run([$middleware], $request);
     }
 
     public function testRequestContextBeingUpdated() : void
@@ -62,9 +103,7 @@ class SymfonyRouterMiddlewareTest extends TestCase
             ->with($symfonyRequest)
             ->willReturn([]);
 
-        $factory = Factory::getResponseFactory();
-
-        $middleware = new SymfonyRouterMiddleware($router, $factory);
+        $middleware = new SymfonyRouterMiddleware($router);
 
         Dispatcher::run([$middleware], $request);
     }
@@ -88,9 +127,7 @@ class SymfonyRouterMiddlewareTest extends TestCase
 
         $p->setValue($this->router, $matcher);
 
-        $factory = Factory::getResponseFactory();
-
-        $middleware = new SymfonyRouterMiddleware($this->router, $factory);
+        $middleware = new SymfonyRouterMiddleware($this->router);
 
         $response = Dispatcher::run([$middleware], $request);
 
@@ -116,9 +153,7 @@ class SymfonyRouterMiddlewareTest extends TestCase
 
         $p->setValue($this->router, $matcher);
 
-        $factory = Factory::getResponseFactory();
-
-        $middleware = new SymfonyRouterMiddleware($this->router, $factory);
+        $middleware = new SymfonyRouterMiddleware($this->router);
 
         $response = Dispatcher::run([$middleware], $request);
 
@@ -140,9 +175,7 @@ class SymfonyRouterMiddlewareTest extends TestCase
 
         $p->setValue($this->router, $matcher);
 
-        $factory = Factory::getResponseFactory();
-
-        $middleware = new SymfonyRouterMiddleware($this->router, $factory);
+        $middleware = new SymfonyRouterMiddleware($this->router);
 
         $response = Dispatcher::run([$middleware], $request);
 
@@ -168,9 +201,7 @@ class SymfonyRouterMiddlewareTest extends TestCase
 
         $p->setValue($this->router, $matcher);
 
-        $factory = Factory::getResponseFactory();
-
-        $middleware = new SymfonyRouterMiddleware($this->router, $factory);
+        $middleware = new SymfonyRouterMiddleware($this->router);
 
         $dummyFn = static function ($request) : void {
             echo $request->getAttribute('_route');
